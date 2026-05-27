fa66df6088458996a2e8c08945a3c2ef2eb7695e
MinhVuong2203 <vuonghihihihi@gmail.com>
2026-05-27 09:23:28 +0700

    Deploy FE

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
+// const api = axios.create({
+//   baseURL: 'http://localhost:7211',
+// })
+
 const api = axios.create({
-  baseURL: 'http://localhost:7211',
+  baseURL: 'https://drivingtestadmin-production.up.railway.app',
 })
 
 export default api
